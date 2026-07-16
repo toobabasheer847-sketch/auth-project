6d0d4e9123d807ca58300a1bd410aeac34b285bf
Tooba <toobabasheer847@gmail.com>
2026-07-16 11:50:52 +0500

    Configure model associations

--- src/models/index.js
+++ src/models/index.js
@@ -1,7 +1,33 @@
 import sequelize from "../config/database.js";
+import User from "./user.model.js";
+import UserOtp from "./userOtp.model.js";
 
-const db = {};
 
-db.sequelize = sequelize;
+// Relationships
+
+
+// One User -> Many OTPs
+User.hasMany(UserOtp, {
+  foreignKey: "userEmail",
+  sourceKey: "email",
+});
+
+// Every OTP belongs to one User
+UserOtp.belongsTo(User, {
+  foreignKey: "userEmail",
+  targetKey: "email",
+});
+
+const db = {
+  sequelize,
+  User,
+  UserOtp,
+};
 
 export default db;
+
+
+
+
+
+
