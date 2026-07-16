b90a0385833f9910dd657299a542372b9162d8d8
Tooba <toobabasheer847@gmail.com>
2026-07-16 11:06:44 +0500

    Configure database connection and startup

---
+++ src/models/index.js
@@ -0,0 +1,7 @@
+import sequelize from "../config/database.js";
+
+const db = {};
+
+db.sequelize = sequelize;
+
+export default db;
--- src/server.js
+++ src/server.js
@@ -1,10 +1,24 @@
 import dotenv from "dotenv";
 import app from "./app.js";
+import sequelize from "./config/database.js";
 
 dotenv.config();
-
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(` Server running on port ${PORT}`);
-});
+async function startServer() {
+  try {
+    await sequelize.authenticate();
+
+    console.log("Database Connected Successfully");
+
+    app.listen(PORT, () => {
+      console.log(` Server running on port ${PORT}`);
+    });
+
+  } catch (error) {
+    console.error(" Database Connection Failed");
+    console.error(error.message);
+  }
+}
+
+startServer();
