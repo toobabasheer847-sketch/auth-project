7f64e64dbdc561713df1135878777b87cbee1567
Tooba <toobabasheer847@gmail.com>
2026-07-16 11:22:45 +0500

    Add User model

---
+++ src/models/user.model.js
@@ -0,0 +1,44 @@
+import { DataTypes } from "sequelize";
+import sequelize from "../config/database.js";
+
+const User = sequelize.define(
+  "User",
+  {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+      unique: true,
+    },
+
+    email: {
+      type: DataTypes.STRING,
+      primaryKey: true,
+      allowNull: false,
+      unique: true,
+    },
+
+    phone: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+
+    verifiedAt: {
+      type: DataTypes.DATE,
+      allowNull: true,
+    },
+  },
+  {
+    tableName: "users",
+    timestamps: true,
+    createdAt: "createdAt",
+    updatedAt: false,
+  }
+);
+
+export default User;
