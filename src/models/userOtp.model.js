3f88213c01a9ff7860c133d6b4b6423d370f44db
Tooba <toobabasheer847@gmail.com>
2026-07-16 11:38:02 +0500

    Add otp model

---
+++ src/models/userOtp.model.js
@@ -0,0 +1,38 @@
+import { DataTypes } from "sequelize";
+import sequelize from "../config/database.js";
+
+const UserOtp = sequelize.define(
+  "UserOtp",
+  {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+      allowNull: false,
+    },
+
+    userEmail: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+
+    otp: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+
+    used: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+      allowNull: false,
+    },
+  },
+  {
+    tableName: "user_otps",
+    timestamps: true,
+    createdAt: "createdAt",
+    updatedAt: false,
+  }
+);
+
+export default UserOtp;
